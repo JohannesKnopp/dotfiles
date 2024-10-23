876a9562c5104baece3df715bb23ae60f74bff2b
Johannes Knopp <johannes.knopp@brainhero.eu>
2024-10-23 14:20:05 +0200

    Fix eslint_d issue

--- nvim/lua/config/plugins/lsp/null-ls.lua
+++ nvim/lua/config/plugins/lsp/null-ls.lua
@@ -1,5 +1,8 @@
 return {
 	'nvimtools/none-ls.nvim',
+	dependencies = {
+		'nvimtools/none-ls-extras.nvim'
+	},
 	config = function()
 		local null_ls = require('null-ls')
 
@@ -9,9 +12,9 @@ return {
 			}),
 			null_ls.builtins.formatting.black,
 			null_ls.builtins.formatting.djlint,
-			null_ls.builtins.formatting.eslint_d,
 			null_ls.builtins.formatting.isort,
 			null_ls.builtins.formatting.stylua,
+			require('none-ls.diagnostics.eslint_d'),
 		}
 
 		null_ls.setup({
